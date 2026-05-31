6ae81bb1daa4724412490152cfc3c88e475a3fe3
waleops <2425401608@live.gctu.edu.gh>
2026-05-31 22:25:34 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,12 @@
-
 def main():
-  pass #code goes here
-
-
+    print("""
+      AAAAA        N       N      QQQQQQQ
+     AA   AA       NN      N     QQ     QQ
+    AA     AA      N N     N    QQ       QQ
+    AAAAAAAAA      N  N    N    QQ       QQ
+    AA     AA      N   N   N    QQ   QQ  QQ
+    AA     AA      N    N  N     QQ    QQQQ
+    AA     AA      N     N N      QQQQQQQ Q
+    """)
 
 main()
